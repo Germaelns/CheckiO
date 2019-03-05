dc085792357b52696e6db9aa961fbd9e2ac78822
Nikolay <nikolay.shevchenko.work@gmail.com>
2019-03-05 21:22:04 +0200

    Update "Fizz Buzz" for work with list

--- Elementary (elementary)/3.fizz buzz.py
+++ Elementary (elementary)/3.fizz buzz.py
@@ -1,13 +1,17 @@
-def checkio(number: int) -> str:
-    if number % 3 is 0 and number % 5 is 0:
-        return "Fizz Buzz"
-    elif number % 5 is 0:
-        return "Buzz"
-    elif number % 3 is 0:
-        return "Fizz"
-    else:
-        return "{}".format(number)
+def checkio(numbers: list) -> list:
+    result = list()
+    for number in numbers:
+        if number % 3 is 0 and number % 5 is 0:
+            result.append("Fizz Buzz")
+        elif number % 5 is 0:
+            result.append("Buzz")
+        elif number % 3 is 0:
+            result.append("Fizz")
+        else:
+            result.append(number)
+
+    return result
 
 
 if __name__ == '__main__':
-    checkio(5)
+    checkio([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
